4d740c755dd6da5dca32f8f41f15499fbf792e79
whichken <23066703+whichken@users.noreply.github.com>
2020-05-14 15:11:01 +0000

    Adds typeorm.

--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,37 @@
 import express from 'express'
 import consola from 'consola'
 import { Nuxt, Builder } from 'nuxt'
+import { createConnection } from 'typeorm'
 const app = express()
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
-async function start() {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+createConnection()
+  .then(async connection => {
+    consola.info(`Database connection established to ${connection.name}`)
 
-  const { host, port } = nuxt.options.server
+    // Init Nuxt.js
+    const nuxt = new Nuxt(config)
 
-  await nuxt.ready()
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  }
+    const { host, port } = nuxt.options.server
 
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
+    await nuxt.ready()
+    // Build only in dev mode
+    if (config.dev) {
+      const builder = new Builder(nuxt)
+      await builder.build()
+    }
 
-  // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
+    // Give nuxt middleware to express
+    app.use(nuxt.render)
+
+    // Listen the server
+    app.listen(port, host)
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
   })
-}
-start()
+  .catch(error => consola.error('Unable to connect to the database.', error))
